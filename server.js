e24d9db81d943b386f4ac339c732e3b2c0e54672
sauliyo15 <gc.saul.7@gmail.com>
2023-03-24 00:18:29 +0100

    server.js mejorado mas sofisticado

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var path = require("path");
+var port = process.env.PORT || 3000;
+var port = 8000;
 
-let app = express();
-let port = 8000;
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(express.static('public'));
-
-app.use('*', function(req, res, next) {
-    res.redirect('/');
+app.use("*", function (req, res, next) {
+  res.redirect("/");
 });
 
-
-app.listen(port, () => {console.log('Servidor arrancado en el puerto: ' + port)});
+http.listen(port, function () {
+  console.log("Open your browser on http://localhost:" + port);
+});
