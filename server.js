93ba42ad85f0194fb4007e69bfe8438c02929f93
sauliyo15 <gc.saul.7@gmail.com>
2023-03-24 19:09:45 +0100

    Servidor: server.js modificado y corregido

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-/*var express = require("express");
-var app = express();
-var http = require("http").Server(app);
-var path = require("path");
-var port = process.env.PORT || 3000;
-var port = 8000;
+'use strict';
+
+const express = require('express');
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+const  path = require("path");
+;
+let app = express();
+let  http = require("http").Server(app);
+
+let port = process.env.PORT || 8000;
+
+
+// Redirect HTTP to HTTPS,
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,43 +21,4 @@ app.use("*", function (req, res, next) {
 
 http.listen(port, function () {
   console.log("Open your browser on http://localhost:" + port);
-});*/
-
-'use strict';
-
-const express = require('express');
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
-
-/**
- * Starts the Express server.
- *
- * @return {ExpressServer} instance of the Express server.
- */
-function startServer() {
-  const app = express();
-
-  // Redirect HTTP to HTTPS,
-  app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
-
-  // Logging for each request
-  app.use((req, resp, next) => {
-    const now = new Date();
-    const time = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
-    const path = `"${req.method} ${req.path}"`;
-    const m = `${req.ip} - ${time} - ${path}`;
-    // eslint-disable-next-line no-console
-    console.log(m);
-    next();
-  });
-
-  // Handle requests for static files
-  app.use(express.static('public'));
-
-  // Start the server
-  return app.listen('8000', () => {
-    // eslint-disable-next-line no-console
-    console.log('Local DevServer Started on port 8000...');
-  });
-}
-
-startServer();
+});
