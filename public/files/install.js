b0ee9d809a1559ccb87bb9cfa10282a08e10d32d
sauliyo15 <gc.saul.7@gmail.com>
2023-03-24 18:29:16 +0100

    Descargar proyecto base

--- public/files/install.js
+++ public/files/install.js
@@ -1,34 +1,59 @@
-'use-strict';
+'use strict';
 
 let deferredInstallPrompt = null;
-
-const installButton = document.getElementById('buttonInstall');
-
+const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
+// CODELAB: Add event listener for beforeinstallprompt event
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
+
+/**
+ * Event handler for beforeinstallprompt event.
+ *   Saves the event & shows install button.
+ *
+ * @param {Event} evt
+ */
 function saveBeforeInstallPromptEvent(evt) {
-    deferredInstallPrompt = evt;
-    installButton.removeAttribute('hidden');
+  // CODELAB: Add code to save event & show the install button.
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
 }
 
+
+/**
+ * Event handler for butInstall - Does the PWA installation.
+ *
+ * @param {Event} evt
+ */
 function installPWA(evt) {
-    deferredInstallPrompt.prompt();
-    evt.srcElement.setAttribute('hidden', true);
-    deferredInstallPrompt.userChoice.then((choice) => {
-        if(choice.outcomie === "accepted") {
-            console.log("aceptado")
-        }
-        else {
-            console.log("no aceptado")
+  // CODELAB: Add code show install prompt & hide the install button.
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.setAttribute('hidden', true);
+  // CODELAB: Log user response to prompt.
+  deferredInstallPrompt.userChoice
+      .then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt', choice);
+        } else {
+          console.log('User dismissed the A2HS prompt', choice);
         }
         deferredInstallPrompt = null;
-    });
+      });
 }
 
+// CODELAB: Add event listener for appinstalled event
 window.addEventListener('appinstalled', logAppInstalled);
 
+/**
+ * Event handler for appinstalled event.
+ *   Log the installation to analytics or save the event somehow.
+ *
+ * @param {Event} evt
+ */
 function logAppInstalled(evt) {
-    console.log("Aplicacion de peliculas instalada");
+  // CODELAB: Add code to log the event
+  console.log('Weather App was installed.', evt);
+
 }
